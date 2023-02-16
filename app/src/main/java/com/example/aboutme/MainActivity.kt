378089c127156b232786d8493d0818e24dd6efaf
Rachel-oc <mengting.xie@thoughtworks.com>
2023-02-16 15:46:46 +0800

    [feat]: use viewStub

--- app/src/main/java/com/example/aboutme/MainActivity.kt
+++ app/src/main/java/com/example/aboutme/MainActivity.kt
@@ -2,10 +2,21 @@ package com.example.aboutme
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.ViewStub
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var stub:ViewStub
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        findViewById<Button>(R.id.more).setOnClickListener { onMoreClick() }
+    }
+
+    private fun onMoreClick(){
+        stub = findViewById(R.id.view_stub)
+        val inflated:View = stub.inflate()
     }
 }
